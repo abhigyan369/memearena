966e3d7c91d6e2a644c45a713a144a2318ce4984
Abhigyan Kumar <abhigyankumar@Abhigyans-MacBook-Air.local>
2026-04-23 09:53:12 +0530

    Fix downvote route with signed integer converter

--- votes/urls.py
+++ votes/urls.py
@@ -1,6 +1,20 @@
-from django.urls import path
+from django.urls import path, register_converter
 from . import views
 
+
+class SignedIntConverter:
+    regex = '-?[0-9]+'
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return str(value)
+
+
+register_converter(SignedIntConverter, 'sint')
+
+
 urlpatterns = [
-    path('<int:meme_id>/<int:value>/', views.vote, name='vote'),
+    path('<int:meme_id>/<sint:value>/', views.vote, name='vote'),
 ]
